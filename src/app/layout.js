299d68c8c865dece1972257d550296985397f7db
Jeffrey Scott Martin <jeffreyscottmartin1974@gmail.com>
2025-02-05 09:49:15 -0500

    inititial setup - branding - fonts - colors - CI/CD

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Fira_Sans, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const firaSans = Fira_Sans({
+  variable: "--font-fira-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -19,9 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${firaSans.variable} ${inter.variable} antialiased`}>
         {children}
       </body>
     </html>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,21 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        display: ["var(--font-inter)", "sans"],
+        body: ["var(--font-fira-sans)", "sans"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        mainBG: "#121212",
+        contentBG: "#1E1E1E",
+        primaryText: "#E0E0E0",
+        secondaryText: "#A0A0A0",
+        accentText: "#FFFFFF",
+        vibrantAccent: "#1E90FF",
+        mutedAccent: "#FF8C00",
+        error: "#FF5C5C",
+        success: "#28A745",
+        seperator: "#2A2A2A",
       },
     },
   },

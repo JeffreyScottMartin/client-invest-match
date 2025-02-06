e0795cb774774296d9f4e6aca9b4355eac50e221
Jeffrey Scott Martin <jeffreyscottmartin1974@gmail.com>
2025-02-05 19:24:39 -0500

    fixed typo

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import LargeHero from "@/app/components/ui/largeHero";
 const Home = () => {
   return (
     <div className="container mt-6 w-3/4 justify-center mb-16 mx-auto">
-      <LargeHero heading="Consulting Redifined" />
+      <LargeHero imageString="/home-hero.png" heading="Consulting Redefined" />
     </div>
   );
 };

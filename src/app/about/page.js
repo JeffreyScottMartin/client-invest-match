bb6f23bccfa68192c7c013a931f7cd4675fdd177
Jeffrey Scott Martin <jeffreyscottmartin1974@gmail.com>
2025-02-06 13:02:05 -0500

    blocks complete, on to layouts

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,37 @@
+import SmallHero from "@/app/components/ui/smallHero";
+import TwoColBlock from "@/app/components/ui/twoColBlock";
+import AboutUsCardGrid from "@/app/components/ui/aboutUsCardGrid";
+import { equiBold } from "@/app/styles/fonts";
 const aboutUs = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Our company is the best!</p>
-    </div>
+    <>
+      <div className="container mt-6 w-10/12 justify-center mb-16 mx-auto">
+        <SmallHero imageString="/about-us-hero.png" heading="Our Journey" />
+      </div>
+      <div className="container mt-6 w-10/12 justify-center mb-4 mx-auto">
+        <TwoColBlock
+          heading={
+            <>
+              <span className="text-lightGray">Empowering Futures:</span>
+              <br />
+              Our Story, Your Success
+            </>
+          }
+          content="InvestMatch was founded by a team of seasoned leaders passionate about solving problems, driving innovation, and providing coaching and mentoring. By combining their expertise and experience, the founders aimed to bridge this gap and empower businesses to thrive while moving beyond traditional consulting."
+        />
+      </div>
+      <div className="container min-w-dvw bg-offwhite flex justify-center py-12 mt-8 mb-8 mx-auto">
+        <AboutUsCardGrid />
+      </div>
+      <div className="container mt-12 w-10/12 justify-center mb-12 mx-auto">
+        <h3
+          className={`${equiBold.className} text-5xl text-center text-darkGray leading-tight mt-20 mb-20`}
+        >
+          <span className="text-lightGray">Driven by </span>
+          Your Success
+        </h3>
+      </div>
+    </>
   );
 };
 
---
+++ src/app/components/ui/aboutUsCardGrid.jsx
@@ -0,0 +1,47 @@
+import { equiSemibold } from "@/app/styles/fonts";
+
+const AboutUsCardGrid = () => {
+  return (
+    <section className="w-10/12 justify-center items-center text-center">
+      <div className="w-[90%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <h3
+            className={`${equiSemibold.className} text-3xl text-darkGray leading-tight text-left`}
+          >
+            Vision Statement
+          </h3>
+          <p className="font-body text-lg text-darkGray text-left sm:text-left mt-2">
+            Thriving economies fueled by the success of bold innovators.
+          </p>
+        </div>
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <h3
+            className={`${equiSemibold.className} text-3xl text-darkGray leading-tight text-left`}
+          >
+            Mission Statement
+          </h3>
+          <p className="font-body text-lg text-darkGray text-left sm:text-left mt-2">
+            To redefine the consulting experience and empower our clients with
+            innovative solutions and transformative learning opportunities.
+          </p>
+        </div>
+        <div className="col-span-2 bg-white rounded-lg shadow-md p-6">
+          <h3
+            className={`${equiSemibold.className} text-3xl text-darkGray leading-tight text-left`}
+          >
+            InvestMatch Ethos
+          </h3>
+          <p className="font-body text-lg text-darkGray text-left mt-2">
+            Our ethos drives our approach to every partnership. We believe in
+            the power of collaboration to achieve shared goals, fostering
+            creativity to spark innovation, prioritizing continuous learning to
+            stay ahead, and embracing fun to create a vibrant, dynamic work
+            environment.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUsCardGrid;
